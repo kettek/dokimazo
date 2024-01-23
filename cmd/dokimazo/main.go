ec8ea5f34fa84adb98900e6b03b03da2f12a7e12
kts of kettek <kts@kettek.net>
2024-01-22 22:49:45 -0800

    Add unfitting icon

--- cmd/dokimazo/main.go
+++ cmd/dokimazo/main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"bytes"
+	_ "embed"
+	"image"
+	_ "image/png"
+
 	"github.com/kettek/dokimazo/internal/game"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
+
+//go:embed icon.png
+var iconBytes []byte
 
 func main() {
 	g := game.New()
 
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 	ebiten.SetWindowTitle("dokimazo")
+
+	icon, _, _ := image.Decode(bytes.NewReader(iconBytes))
+	ebiten.SetWindowIcon([]image.Image{icon})
 
 	if err := ebiten.RunGame(g); err != nil {
 		panic(err)
